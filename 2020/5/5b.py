b6529ffcdcdc3853754ee153a9d1a7f2cbd05385
Victor Azizi <victor@lipsum.eu>
2020-12-05 13:02:59 +0100

    two line 5b

--- 2020/5/5b.py
+++ 2020/5/5b.py
@@ -1,13 +1,15 @@
-with open("input", "r") as f:
-  tickets = f.readlines()
-  seat_ids = []
-  for ticket in tickets:
-    row_raw = list(ticket[:7])
-    row = sum([2**i for i in range(7) if row_raw[-i -1] == 'B'])
-    seat_raw = list(ticket[7:-1])
-    seat= sum([2**i for i in range(3) if seat_raw[-i -1] == 'R'])
-    seat_ids.append(seat + row*8)
-  seat_ids = sorted(seat_ids)
-  for i in range(1,len(seat_ids)):
-    if seat_ids[i-1] != seat_ids[i] -1:
-      print(seat_ids[i-1:i+1],sum(seat_ids[i-1:i+1])/2)
+seat_ids = sorted (
+  [  
+    sum([2**i for i in range(7) if list(ticket[:7])[-i -1] == 'B']) * 8
+  + sum([2**i for i in range(3) if list(ticket[7:-1])[-i -1] == 'R'])
+    for ticket in open("input", "r").readlines()
+  ]
+)
+
+print (
+  [ sum(seat_ids[i-1:i+1])/2 
+    for i in range(1,len(seat_ids)) 
+    if seat_ids[i-1] != seat_ids[i] - 1
+  ]
+)
+

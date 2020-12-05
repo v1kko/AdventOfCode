c6e9704e8be23c315f7a0358d41c4204ca5f5532
Victor Azizi <victor@lipsum.eu>
2020-12-05 12:56:54 +0100

    onlined 5a

--- 2020/5/5a.py
+++ 2020/5/5a.py
@@ -1,10 +1,9 @@
-with open("input", "r") as f:
-  tickets = f.readlines()
-  seat_ids = []
-  for ticket in tickets:
-    row_raw = list(ticket[:7])
-    row = sum([2**i for i in range(7) if row_raw[-i -1] == 'B'])
-    seat_raw = list(ticket[7:-1])
-    seat= sum([2**i for i in range(3) if seat_raw[-i -1] == 'R'])
-    seat_ids.append(seat + row*8)
-  print(max(seat_ids))
+print(
+  max(
+    [  
+      sum([2**i for i in range(7) if list(ticket[:7])[-i -1] == 'B']) * 8
+    + sum([2**i for i in range(3) if list(ticket[7:-1])[-i -1] == 'R'])
+      for ticket in open("input", "r").readlines()
+    ]
+  )
+)

5179dc7a29e41f58368e235f00f04bd53ca8d271
Victor Azizi <victor@lipsum.eu>
2020-12-06 09:54:15 +0100

    one-liner for 6a.py

--- 2020/6/6a.py
+++ 2020/6/6a.py
@@ -1,3 +1,12 @@
-with open("input","r") as f:
-  groups = [ set(list("".join(group.split()))) for group in f.read().split('\n\n')]
-  print (sum([len(group) for group in groups]))
+print(
+  sum(
+    [
+      len(
+        set(
+            "".join(group.split())
+        )
+      ) 
+      for group in open("input","r").read().split('\n\n')
+    ]
+  )
+)

155a809a613b1b071c74667a10906c390404a858
Victor Azizi <victor@lipsum.eu>
2023-12-24 22:30:10 +0100

    2023 12

---
+++ 2023/12.f90
@@ -0,0 +1,170 @@
+subroutine solver12(part,input,ans)
+  implicit none
+  integer(int64) :: ans
+  type(char_p), target :: input(:)
+  integer :: part
+  ans = 0
+  select case(part)
+  case(1)
+    call solver_a(input,ans)
+  case(2)
+    call solver_b(input,ans)
+  end select
+contains
+subroutine solver_a(input,ans)
+  implicit none
+  integer(int64) :: ans
+  type(char_p), target :: input(:)
+  character(len=:), pointer :: p, r
+  integer :: n, s, d
+  integer :: nums(100), nn
+  ans = 0
+
+  do n =1,size(input)
+    p => input(n)%p
+    nn = 0
+    s = scan(p,' ') + 1
+    r => p(:s-2)
+    do
+      d = scan(p(s:),',')
+      if (d == 0) then
+        d = len(p)
+      else 
+        d = d + s -2
+      end if
+      nn = nn + 1
+      read(p(s:d),*) nums(nn)
+      if (d == len(p)) exit
+      s = d + 2
+    end do
+    s = scan(r,'#?')
+    if (s == 0) cycle
+    r => r(s:)
+    ans = ans + solve_a(r,nums(:nn))
+
+  end do
+end subroutine
+recursive function solve_a(p, nums) result(possible)
+  implicit none
+  character(len=:), pointer :: p,r,q
+  integer :: nums(:)
+  integer :: possible, cur, x
+  possible = 0
+  r => p
+  cur = nums(1)
+  do
+    if (sum(nums)+(size(nums)-1) > len(r)) exit
+
+    x = scan(r(:cur),'.')
+    if (x /= 0) goto 2
+
+    if (cur < len(r)) then
+      if ( r(cur+1:cur+1) == '#') goto 2
+    end if
+
+    if (size(nums) == 1) then
+      if (scan(r(cur+2:),'#') == 0) possible = possible + 1
+    else
+      x = scan(r(cur+2:),'?#')
+      if ( x == 0 ) exit
+      q => r(cur+1+x:)
+      possible = possible + solve_a(q,nums(2:))
+    end if
+2   continue
+    if (r(1:1) == '#') exit
+    x = scan(r(2:),'#?')
+    if (x==0) exit
+    r => r(x+1:)
+  end do
+end function
+subroutine solver_b(input,ans)
+  implicit none
+  integer(int64) :: ans
+  type(char_p), target :: input(:)
+  character(len=:), pointer :: p, r
+  character(len=:), allocatable, target :: x
+  integer :: n, s, d
+  integer :: nums(100), nn, ncache(100)
+  integer(int64), allocatable :: worth(:,:)
+  integer, allocatable :: cache(:,:)
+  ans = 0
+
+  allocate(cache(100,100))
+  allocate(worth(100,100))
+
+  do n =1,size(input)
+    p => input(n)%p
+    nn = 0
+    s = scan(p,' ') + 1
+    x = p(:s-2)
+    do
+      d = scan(p(s:),',')
+      if (d == 0) then
+        d = len(p)
+      else 
+        d = d + s -2
+      end if
+      nn = nn + 1
+      read(p(s:d),*) nums(nn)
+      if (d == len(p)) exit
+      s = d + 2
+    end do
+    do s = 1,5
+      nums(s*nn+1:(s+1)*nn)=nums(:nn)
+    end do
+    nn = nn*5 
+    x = x // '?' // x // '?' // x // '?' // x // '?' // x
+    r => x
+    s = scan(r,'#?')
+    if (s == 0) cycle
+    r => r(s:)
+
+    ncache = 0
+    ans = ans + solve_b(r,nums(:nn),cache(:,:nn),worth(:,:nn),ncache(:nn))
+  end do
+end subroutine
+recursive function solve_b(p, nums, cache, worth, ncache) result(possible)
+  implicit none
+  character(len=:), pointer :: p,r,q
+  integer :: nums(:)
+  integer(int64) :: worth(:,:), possible
+  integer :: cur, x, ncache(:), n
+  integer :: cache(:,:)
+  possible = 0
+  r => p
+  cur = nums(1)
+  do n = 1,ncache(1)
+    if (len(p) == cache(n,1)) then
+      possible = worth(n,1)
+      return
+    end if
+  end do
+  do
+    if (sum(nums)+(size(nums)-1) > len(r)) exit
+
+    x = scan(r(:cur),'.')
+    if (x /= 0) goto 2
+
+    if (cur < len(r)) then
+      if ( r(cur+1:cur+1) == '#') goto 2
+    end if
+
+    if (size(nums) == 1) then
+      if (scan(r(cur+2:),'#') == 0) possible = possible + 1
+    else
+      x = scan(r(cur+2:),'?#')
+      if ( x == 0 ) exit
+      q => r(cur+1+x:)
+      possible = possible + solve_b(q,nums(2:),cache(:,2:),worth(:,2:),ncache(2:))
+    end if
+2   continue
+    if (r(1:1) == '#') exit
+    x = scan(r(2:),'#?')
+    if (x==0) exit
+    r => r(x+1:)
+  end do
+  ncache(1) = ncache(1) + 1
+  cache(ncache(1),1) = len(p)
+  worth(ncache(1),1) = possible
+end function
+end subroutine
--- 2023/aoc_2023.f90
+++ 2023/aoc_2023.f90
@@ -38,7 +38,7 @@ contains
 #include "9.f90"
 #include "10.f90"
 #include "11.f90"
-!#include "12.f90"
+#include "12.f90"
 !include "13.f90"
 !include "14.f90"
 !include "15.f90"
@@ -66,7 +66,7 @@ implicit none
   solver_list(9)%p => solver9
   solver_list(10)%p => solver10
   solver_list(11)%p => solver11
-  !solver_list(12)%p => solver12
+  solver_list(12)%p => solver12
   !solver_list(13)%p => solver13
   !solver_list(14)%p => solver14
   !solver_list(15)%p => solver15
